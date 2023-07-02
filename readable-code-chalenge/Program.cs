c0f9d43a378a9368b1eef33441d06791bb10d8c8
vinicgobbi <vinicius.cgobbi2004@gmail.com>
2023-07-02 20:46:04 -0300

    Adicionado mais atividades

---
+++ readable-code-chalenge/Program.cs
@@ -0,0 +1,20 @@
+﻿/* This program reverses a message
+and count the letter 'o' and print this on console
+*/
+string str = "The quick brown fox jumps over the lazy dog.";
+
+char[] message = str.ToCharArray();
+Array.Reverse(message);
+
+int x = 0;
+
+foreach (char i in message) {
+    if (i == 'o') {
+        x++; 
+    }
+}
+
+string new_message = new String(message);
+
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
