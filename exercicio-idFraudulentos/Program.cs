181855ce5132cf5f197d45a167790dc8b7a188bb
vinicgobbi <vinicius.cgobbi2004@gmail.com>
2023-07-02 18:12:00 -0300

    Adicionados novas atividades

---
+++ exercicio-idFraudulentos/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] fraudulentIDs = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+
+foreach (string id in fraudulentIDs){
+    if(id.StartsWith("B")){
+        Console.WriteLine(id);
+    }
+}
