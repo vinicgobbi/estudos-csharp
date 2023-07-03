4a7833437c380d92221ef69bc25581d84ed48435
vinicgobbi <vinicius.cgobbi2004@gmail.com>
2023-07-03 15:11:44 -0300

    Finalizada a atividade 'arrays-iteration-selection'

--- arrays-iteration-selection/Program.cs
+++ arrays-iteration-selection/Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 
 // initialize variables - graded assignments 
-int currentAssignments = 5;
+int examAssignments = 5;
 
-string[] studentsNames = new string[] {"Sophia", "Andrew", "Emma", "Logan"};
 string currentStudentLetterGrade = "";
 
 int[] studentScores = new int[10];
@@ -12,12 +11,17 @@ int[] sophiaScores = new int[] {90, 86, 87, 98, 100, 94, 90};
 int[] andrewScores = new int[] {92, 89, 81, 96, 90, 89};
 int[] emmaScores = new int[] {90, 85, 87, 98, 68, 89, 89, 89};
 int[] loganScores = new int[] {90, 95, 87, 88, 96, 96};
+int[] beckyScores = new int[] {92, 91, 90, 91, 92, 92, 92};
+int[] chrisScores = new int[] {84, 86, 88, 90, 92, 94, 96, 98};
+int[] ericScores = new int[] {80, 90, 100, 80, 90, 100, 80, 90};
+int[] gregorScores = new int[] {91, 91, 91, 91, 91, 91, 91};
+
+string[] studentsNames = new string[] {"Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor"};
 
 Console.WriteLine("Student\t\tGrade\n");
 
 
 foreach(string name in studentsNames){
-    
     string currentStudent = name;
 
     if (currentStudent == "Sophia")
@@ -28,15 +32,29 @@ foreach(string name in studentsNames){
         studentScores = emmaScores;
     else if (currentStudent == "Logan")
         studentScores = loganScores;
+    else if (currentStudent == "Becky")
+        studentScores = beckyScores;
+    else if (currentStudent == "Chris")
+        studentScores = chrisScores;
+    else if (currentStudent == "Eric")
+        studentScores = ericScores;
+    else if (currentStudent == "Gregor")
+        studentScores = gregorScores;
     
     int sumAssignmentScores = 0; 
     decimal currentStudentGrade = 0;
+    int gradedAssignments = 0;
     
     foreach (int score in studentScores){
-        sumAssignmentScores += score;
+        gradedAssignments++;
+        if(gradedAssignments <= examAssignments){
+            sumAssignmentScores += score;
+        }else{
+            sumAssignmentScores += score / 10;
+        }
     }
 
-    currentStudentGrade = (decimal)sumAssignmentScores / currentAssignments;
+    currentStudentGrade = (decimal)sumAssignmentScores / examAssignments;
 
     if(currentStudentGrade >= 97){
         currentStudentLetterGrade = "A+";
